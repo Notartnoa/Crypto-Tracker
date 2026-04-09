b4b03febb7b312772747c143bc8d96f0bdd5598f
Notartnoa <pradanamauludin@gmail.com>
2026-04-09 11:36:09 +0700

    Tampilan Baru

--- app/Http/Controllers/CryptoController.php
+++ app/Http/Controllers/CryptoController.php
@@ -7,17 +7,46 @@ use Illuminate\Support\Facades\Http;
 
 class CryptoController extends Controller
 {
+    // Halaman utama
     public function index()
     {
         $response = Http::get('https://api.coingecko.com/api/v3/coins/markets', [
-            'vs_currency' => 'usd',
-            'ids' => 'bitcoin,ethereum,solana,binancecoin,ripple,cardano,dogecoin,polkadot,fartcoin,mantra, ethena, pendle',
-            'order'       => 'market_cap_desc',
-            'sparkline'   => false,
+            'vs_currency'           => 'usd',
+            'order'                 => 'market_cap_desc',
+            'per_page'              => 20,
+            'page'                  => 1,
+            'sparkline'             => false,
+            'price_change_percentage' => '24h',
         ]);
 
         $coins = $response->json();
 
         return view('crypto.index', compact('coins'));
+    }
+
+    // Search koin (dipanggil via AJAX)
+    public function search(Request $request)
+    {
+        $query = $request->get('q', '');
+
+        $response = Http::get('https://api.coingecko.com/api/v3/search', [
+            'query' => $query,
+        ]);
+
+        $results = collect($response->json()['coins'] ?? [])->take(8);
+
+        return response()->json($results);
+    }
+
+    public function chart($id, Request $request)
+    {
+        $days = $request->get('days', 7);
+
+        $response = Http::get("https://api.coingecko.com/api/v3/coins/{$id}/ohlc", [
+            'vs_currency' => 'usd',
+            'days'        => $days,
+        ]);
+
+        return response()->json($response->json());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CryptoController;
 
 Route::get('/', [CryptoController::class, 'index']);
+Route::get('/search', [CryptoController::class, 'search']);
+Route::get('/chart/{id}', [CryptoController::class, 'chart']);
